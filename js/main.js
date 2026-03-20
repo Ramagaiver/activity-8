545845eb8375814f11199d784e103db553a93b92
Ramagaiver <chanodomtannarat@gmail.com>
2026-03-19 19:09:04 -0500

    Lesson 3

--- js/main.js
+++ js/main.js
@@ -42,7 +42,39 @@ window.onload = function(){
         }
     ];
 
-    var cityCircles = container.selectAll(".circles")
+    // Scale for circle center x coordinate
+    var x = d3.scaleLinear() 
+        .range([90, 810]) // Output min and max
+        .domain([0, 3]); // Input min and max
+
+    // Minimum population value of in the CityPop array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    // Maximum population value of in the CityPop array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    // Scale for circle center y coordinate
+    var y = d3.scaleLinear()
+        .range([450, 50])
+        .domain([0, 700000]);
+
+    // Color ramp
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+
+        .domain([
+            minPop, 
+            maxPop
+        ]);
+        
+    var cityCircles = container.selectAll(".circles")        
         .data(cityPop) // Feeds the cityPop array
         .enter() // Joins the cityPop array
         .append("circle")
@@ -60,13 +92,73 @@ window.onload = function(){
             return Math.sqrt(area/Math.PI);
         })
 
-        // Circle X coordinate
+        // Uses the scale generator to position circles horizontally
         .attr("cx", function(d, i){
-            return 90 + (i * 180);
+            return x(i);
         })
 
-        // Circle y coordinate
+        // Uses the scale generator to position circles vertically
         .attr("cy", function(d){
-            return 450 - (d.population * 0.0005);
+            return y(d.population);
+        })
+
+        .style("fill", function(d, i){
+            return color(d.population);
+        })
+
+        .style("stroke", "#000");
+
+    var yAxis = d3.axisLeft(y);
+
+    // Rotates the axis by 50 pixels to the right
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50, 0)")
+        .call(yAxis);
+
+    // Title
+    var title = container.append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "middle")
+        .attr("x", 450)
+        .attr("y", 30)
+        .text("Wisconsin City Populations");
+
+    // Creates cityCircles labels
+    var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y", function(d){
+            // Vertical position centered on each circle
+            return y(d.population) + 5;
         });
+
+    // First line of cityCircles labels
+    var nameLine = labels.append("tspan")
+        .attr("class", "nameLine")
+        .attr("x", function(d,i){
+            // Horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .text(function(d){
+            return d.city;
+        });
+
+    var format = d3.format(",");
+
+    // Second line of cityCircles labels
+    var popLine = labels.append("tspan")
+        .attr("class", "popLine")
+        .attr("x", function(d,i){
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .attr("dy", "15") // Vertical offset
+        .text(function(d){
+            return "Pop. " + format(d.population);
+        });
+        
+    yAxis(axis);
 };
