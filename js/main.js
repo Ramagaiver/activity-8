3a576c8cf62fef65c85a50878ebb23636b54df8d
Ramagaiver <chanodomtannarat@gmail.com>
2026-03-26 18:55:24 -0500

    Lesson 2 - Activity 9

--- js/main.js
+++ js/main.js
@@ -2,27 +2,67 @@ window.onload = setMap;
 
 function setMap(){
 
+    // This sets initial width and height, although its probably obsolete due to CSS styles
+    var width = 900,
+        height = 900;
+
+    // Creates SVG containers for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    // Handles Albers equal area conic projection
+    // Technically the Royal Thai Survey Department promotes the use of UTM, 
+    // but I'd rather use something more stylistically pleasing and non-standard
+    var projection = d3.geoAlbers()
+        .center([160, 18.5])
+        .rotate([66.45, -0.91, 0])
+        .parallels([0.00, 25.00])
+        .scale(4000)
+        .translate([width / 2, height / 2]);
+        
+    // Applies the projection to the data
+    var path = d3.geoPath()
+        .projection(projection);
+
     // Asynchronously loads all data
-    var promises = [d3.csv("data/FORMAL vs INFORMAL Employment in Thailand 2024 by Province.csv"),                    
-                    d3.json("data/Countries.topojson"),                    
-                    d3.json("data/Provinces2.topojson")                   
-                    ];    
-        Promise.all(promises).then(callback);    
+    var promises = [
+            d3.csv("data/FORMAL vs INFORMAL Employment in Thailand 2024 by Province.csv"),                    
+            d3.json("data/Countries.topojson"),                    
+            d3.json("data/Provinces2.topojson")                   
+        ];  
+    Promise.all(promises).then(function(data) {
+            // Pass map and path to the callback   
+            callback(data, map, path);
+        });
+}
 
-        // Gets the data
-        function callback(data){    
-            csvData = data[0];    
-            asia = data[1];    
-            thailand = data[2];
-            console.log(csvData);
-            console.log(asia);
-            console.log(thailand);    
+// Function responsible for getting all necessary data
+function callback(data, map, path){    
+    var csvData = data[0],    
+        asia = data[1],    
+        thailand = data[2];
 
-            // TopoJson -> GeoJson
-            var asianCountries = topojson.feature(asia, asia.objects.Countries),
-                thaiProvinces = topojson.feature(thailand, thailand.objects.Provinces2);
+    // TopoJSON -> GeoJSON
+    var asianCountries = topojson.feature(asia, asia.objects.Countries),
+        thaiProvinces = topojson.feature(thailand, thailand.objects['Provinces2']).features;
 
-            console.log(asianCountries);
-            console.log(thaiProvinces);
-        };
+    // Renders background countries (Countries.topojson)
+    var countries = map.append("path")
+        .datum(asianCountries)
+        .attr("class", "countries")
+        .attr("d", path);
+
+    // Draws Thai provinces (Provinces2.topojson)
+    var provinces = map.selectAll(".provinces")
+        .data(thaiProvinces)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            // This assigns a class for CSS and an ID
+            return "provinces " + d.properties.name; 
+        })
+        .attr("d", path);
 };
